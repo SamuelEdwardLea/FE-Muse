1c62d64bb73ae6687335915497d9595dd9532ca8
Paul Copley <pkcopley@gmail.com>
2018-01-31 17:19:53 +0000

    Add CSS to Login component

--- muse-frontend/src/Login.js
+++ muse-frontend/src/Login.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+
+const muiTheme = getMuiTheme(lightBaseTheme);
+
 
 const Login = () => {
   return (
-    <div>
-      <img src={require('./Spotify_Logo_RGB_Green.png')} style={{height:' 30px'}}/>
-      <a href="http://localhost:3000/api/authorise"><button>Log in</button></a>
+    <MuiThemeProvider muiTheme={muiTheme}>
+    <div className="App">
+      <div className="container" id="backDiv">
+      <div className="loginContainer">   
+      <p>Muse</p>    
+      <p style={{fontSize: '50px'}}>A dating app for</p>     
+      <img src={require('./Spotify_Logo_RGB_Green.png')} style={{height:' 125px'}}/>
+      <p></p>
+      <a href="http://localhost:3000/api/authorise"><RaisedButton label="Log In" backgroundColor="rgb(38, 206, 44)"/></a>
+      <p></p>
+      <p></p>
+      </div>
+      </div>
     </div>
+    </MuiThemeProvider>
   )
 }
 
