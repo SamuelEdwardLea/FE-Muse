6c2e63ae9f64679bd32b72ec9d17ed0f48998f47
Megan Field <megan.field@hotmail.co.uk>
2018-02-01 15:07:25 +0000

    interested in and age range css ordered, with labels added

--- muse-frontend/src/Navbar.js
+++ muse-frontend/src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div>
       <div className="nav-bar" style={{display: "grid", gridTemplateColumns: "repeat(10, 1fr)", alignSelf: "center" }}>
-        <div style={{ gridColumn: "1", marginLeft: "2vw", marginRight: "9vw", marginTop: "1vh" }}>
+        <div style={{ gridColumn: "1", marginLeft: "2vw", marginRight: "9vw", marginTop: "1px" }}>
           <img style={{ height: '50px', width: '50px', display: 'inline-block' }} src={require("./Spotify_Icon_RGB_Green.png")} alt="Spotify" />
         </div>
         <NavLink exact to="/profile" style={{ gridColumn: "2", margin: "auto"}}><img src="https://image.flaticon.com/icons/svg/118/118781.svg" style={{ height: '40px', width: "auto" }}></img></NavLink>
